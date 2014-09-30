08b323d25e5bdb48708fdc375e8387d6f4df715e
Aditya Prakash <aditya.prakash132@gmail.com>
2014-09-30 09:08:34 +0530

    scroll added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140911052657) do
+ActiveRecord::Schema.define(:version => 20140923152515) do
 
   create_table "indices", :force => true do |t|
     t.datetime "created_at", :null => false
